b8d0a0d567442f58186bc3297546a362ca3de8e9
Rishabh Chaturvedi <chaturvedirishabh91@gmail.com>
2018-01-30 16:29:05 +0530

    photoLibraryUsage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { PhotoLibrary } from '@ionic-native/photo-library';
+import { Crop } from '@ionic-native/crop';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -22,6 +26,8 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
+    Crop,
+    PhotoLibrary,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PhotoLibrary } from '@ionic-native/photo-library';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  vari : any ;
+  constructor(public navCtrl: NavController, public photoLibrary : PhotoLibrary) {
 
-  constructor(public navCtrl: NavController) {
+  }
 
+  ionViewDidLoad(){
+    this.photoLibrary.requestAuthorization().then(() => {
+      this.photoLibrary.getLibrary().subscribe({
+        next: library => {
+          library.forEach(function(libraryItem) {
+            this.vari = libraryItem;
+            console.log(libraryItem.id);          // ID of the photo
+            console.log(libraryItem.photoURL);    // Cross-platform access to photo
+            console.log(libraryItem.thumbnailURL);// Cross-platform access to thumbnail
+            console.log(libraryItem.fileName);
+            console.log(libraryItem.width);
+            console.log(libraryItem.height);
+            console.log(libraryItem.creationDate);
+            console.log(libraryItem.latitude);
+            console.log(libraryItem.longitude);
+            console.log(libraryItem.albumIds);    // array of ids of appropriate AlbumItem, only of includeAlbumsData was used
+          });
+        },
+        error: err => { console.log('could not get photos'); },
+        complete: () => { console.log('done getting photos'); }
+      });
+    })
+    .catch(err => console.log('permissions weren\'t granted'));
   }
 
 }
